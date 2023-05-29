d52013aea465b7c2bf3353a584bbcf88a0e89b93
Radu Lefter <lefter_radu@yahoo.com>
2023-05-29 21:35:24 +0100

    created basic canvas figures

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-window.addEventListener("load", function(){
-    const canvas = document.getElementById("canvas1");
-})
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth * 0.8;
+  canvas.height = window.innerHeight * 0.8;
+
+  //canvas settings
+  ctx.fillStyle = 'green';
+  ctx.strokeStyle = 'yellow';
+  ctx.lineWidth = 40;
+  ctx.lineCap = "round";
+
+  //effect settings
+  let size = 100;
+
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.moveTo(canvas.width/2, canvas.height/2);
+  ctx.lineTo(size, canvas.height/2);
+  ctx.stroke();
+});
