64668ecb89cbad2d3123d77c0d5d644472657825
Radu Lefter <lefter_radu@yahoo.com>
2023-06-01 21:51:26 +0100

    created function to create fractal

--- script.js
+++ script.js
@@ -5,27 +5,55 @@ window.addEventListener('load', function () {
   canvas.height = window.innerHeight * 0.8;
 
   //canvas settings
-  ctx.fillStyle = 'green';
+  ctx.fillStyle = 'red';
   ctx.strokeStyle = 'yellow';
-  ctx.lineWidth = 30;
+  ctx.lineWidth = 10;
   ctx.lineCap = 'round';
 
   //effect settings
   let size = 200;
-  let sides = 14;
+  let sides = 5;
+  let maxLevel = 3;
+  let scale = 0.5;
+  let spread = 0.8;
+  let branches = 2;
   ctx.save();
   ctx.translate(canvas.width / 2, canvas.height / 2);
   ctx.scale(1, 1);
   ctx.rotate(0);
   //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  for(let i = 0; i < sides; i++){
+  function drawBranch(level){
+    if(level > maxLevel) return;
     ctx.beginPath();
     ctx.moveTo(0, 0);
     ctx.lineTo(size, 0);
     ctx.stroke();
-    ctx.rotate((Math.PI/2)/sides);
+    for(let i = 0; i < branches; i++){
+      ctx.save();
+      ctx.translate(size - (size/branches) * i, 0)
+      ctx.rotate(spread);
+      ctx.scale(scale, scale)
+      drawBranch(level+1)
+      ctx.restore();
+  
+      ctx.save();
+      ctx.translate(size - (size/branches) * i, 0)
+      ctx.rotate(-spread);
+      ctx.scale(scale, scale)
+      drawBranch(level+1)
+      ctx.restore();
+    }
+
+
   }
+
+  drawBranch(0)
+
+  // for(let i = 0; i < sides; i++){
+    
+  //   ctx.rotate((Math.PI/2)/sides);
+  // }
   
  
 
