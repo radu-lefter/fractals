327901363cd57f6317fb4445d25d618ad827e7c2
Radu Lefter <lefter_radu@yahoo.com>
2023-06-02 23:02:01 +0100

    created function to draw fractal

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
   canvas.width = window.innerWidth * 0.8;
-  canvas.height = window.innerHeight * 0.8;
+  canvas.height = window.innerHeight * 1;
 
   //canvas settings
   ctx.fillStyle = 'red';
@@ -11,51 +11,47 @@ window.addEventListener('load', function () {
   ctx.lineCap = 'round';
 
   //effect settings
-  let size = 200;
+  let size = 100;
   let sides = 5;
   let maxLevel = 3;
   let scale = 0.5;
   let spread = 0.8;
   let branches = 2;
-  ctx.save();
-  ctx.translate(canvas.width / 2, canvas.height / 2);
-  ctx.scale(1, 1);
-  ctx.rotate(0);
-  //ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  function drawBranch(level){
-    if(level > maxLevel) return;
+  function drawBranch(level) {
+    if (level > maxLevel) return;
     ctx.beginPath();
     ctx.moveTo(0, 0);
     ctx.lineTo(size, 0);
     ctx.stroke();
-    for(let i = 0; i < branches; i++){
+    for (let i = 0; i < branches; i++) {
       ctx.save();
-      ctx.translate(size - (size/branches) * i, 0)
+      ctx.translate(size - (size / branches) * i, 0);
       ctx.rotate(spread);
-      ctx.scale(scale, scale)
-      drawBranch(level+1)
+      ctx.scale(scale, scale);
+      drawBranch(level + 1);
       ctx.restore();
-  
+
       ctx.save();
-      ctx.translate(size - (size/branches) * i, 0)
+      ctx.translate(size - (size / branches) * i, 0);
       ctx.rotate(-spread);
-      ctx.scale(scale, scale)
-      drawBranch(level+1)
+      ctx.scale(scale, scale);
+      drawBranch(level + 1);
       ctx.restore();
     }
-
-
   }
 
-  drawBranch(0)
+  function drawFractal() {
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.scale(1, 1);
+    ctx.rotate(0);
+    for (let i = 0; i < sides; i++) {
+      ctx.rotate(Math.PI * 2 / sides);
+      drawBranch(0);
+    }
+    ctx.restore();
+  }
 
-  // for(let i = 0; i < sides; i++){
-    
-  //   ctx.rotate((Math.PI/2)/sides);
-  // }
-  
- 
-
-  ctx.restore();
+  drawFractal();
 });
